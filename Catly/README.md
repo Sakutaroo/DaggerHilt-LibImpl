33dd7b741784d4cec91b4ea0ef0ee7e9b0be3c5d
Sakutaroo <kevin.demy@epitech.eu>
2024-04-29 20:59:07 +0200

    [ADD] Catly: project foundation

---
+++ Catly/README.md
@@ -0,0 +1,5 @@
+# Catly
+
+This project aims to implement the **Retrofit** library using Dagger Hilt for dependency injection.
+
+Retrofit is a powerful and easy-to-use networking library for Android applications. It allows you to create interfaces that define HTTP requests and send them.
---
+++ Catly/app/src/main/java/fr/sakutaroo/catly/MainActivity.kt
@@ -0,0 +1,25 @@
+package fr.sakutaroo.catly
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import fr.sakutaroo.catly.ui.theme.CatlyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CatlyTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
